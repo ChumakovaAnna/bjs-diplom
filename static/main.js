3962b4d79c316d2e4a7d47e5d94626267cde6bec
Anna Chumakova <chumakova3008@gmail.com>
2020-01-30 23:22:42 +0500

    Правки в методы конструктора

--- static/main.js
+++ static/main.js
@@ -16,74 +16,106 @@ class Profile {
   }
 
   creatUser(callback) {
-    console.log(`Пользователь ${this.username} создан.`);
     return ApiConnector.createUser(
       {
         username: this.username,
         name: {
           firstName: this.firstName,
           lastName: this.lastName
-          },
+        },
         password: this.password
       },
       (err, data) => {
+        console.log(`Пользователь ${this.username} создан.`);
         callback(err, data);
       });
   }
 
   performLogin(callback) { 
-    console.log(`Пользователь ${this.username} авторизован.`);
     return ApiConnector.performLogin(
       {
         username: this.username,
         password: this.password,
       },
       (err, data) => {
+        console.log(`Пользователь ${this.username} авторизован.`);
         callback(err, data)
       });
   }
 
   addMoney({currency, amount}, callback){
-    console.log(`Добавлено ${amount} ${currency} в кошелек ${this.username}`);
     return ApiConnector.addMoney({currency, amount}, (err, data) => {
+      console.log(`Добавлено ${amount} ${currency} в кошелек ${this.username}`);
       callback(err, data);
       });
   }
 
   convertMoney({fromCurrency, targetCurrency, targetAmount}, callback) {
-    console.log(`Перевели ${targetAmount} ${targetCurrency} из ${fromCurrency}`);
     return ApiConnector.convertMoney({fromCurrency, targetCurrency, targetAmount}, (err, data) => {
+      console.log(`Перевели ${targetAmount} ${targetCurrency} из ${fromCurrency}`);
       callback(err, data)
     });
   }
 
   transferMoney({to, amount}, callback) {
-    console.log(`Переведено ${amount} токенов пользователю ${to}`);
     return ApiConnector.transferMoney({to, amount}, (err, data) => {
+      console.log(`Переведено ${amount} токенов пользователю ${to}`);
       callback(err, data);
     });
   }
-}
+};
 
-const exchangeRates = function (callback) {
-  console.log(`Получили курс валют`);
+const getStocks = function (callback) {
   return getStocks = ApiConnector.getStocks((err, date) => {
+    console.log(`Получили курс валют`);
     callback(err, date);
   });
-}
-
-let oleg = {
-  username: `oleg12`,
-  name: {
-    firstName: `Oleg`,
-    lastName: `Braun`},
-  password: 1233
 };
-console.log(oleg);
 
-const firstUser = new Profile(oleg);
-console.log(firstUser);
+function main () {
+  const firstUser = new Profile(
+    {
+      username: "Oleg First",
+      name: {
+        firstName: "Oleg",
+        lastName: "Versev"
+        },
+      password: "12345"
+  });
 
-firstUser.creatUser();
+  const userSecond = new Profile(
+    {
+      username: "Sveta Second",
+      name: {
+        firstName: "Sveta",
+        lastName: "Nevolina"
+        },
+      password: "54321"
+    });
 
-console.log(getStocks);
+    firstUser.creatUser((err, date) => {
+      if (err) {
+        console.error(`An error occurred during creating ${firstUser}`);
+      } else {
+
+        firstUser.performLogin((err, date) => {
+          if (err) {
+            console.error(`An error occurred during authorization ${firstUser}`);
+          } else {
+
+            firstUser.addMoney({currency: `EUR`, amount: 200}, (err, date) => {
+              if (err) {
+                console.error(`Error during adding money to ${firstUser}`);
+              } else {
+
+                // firstUser.convertMoney({fromCurrency: `EUR`, targetCurrency: 100, targetAmount: `EUR`});
+              };
+            });
+          };
+        });
+      };
+    });
+
+};
+
+main();
